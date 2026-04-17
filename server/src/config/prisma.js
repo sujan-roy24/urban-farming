851863ffba3c7699664918817e84837dcd5e74f3
sujan-roy24 <sjnry25@gmail.com>
2026-04-17 13:53:32 +0600

    fix(prisma): configure postgres adapter for prisma client

--- server/src/config/prisma.js
+++ server/src/config/prisma.js
@@ -1,5 +1,10 @@
+const { PrismaPg } = require("@prisma/adapter-pg");
 const { PrismaClient } = require("../generated/prisma");
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
